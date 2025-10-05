792895e7145cbc60bf427a39a2ad62cfec16f891
Dmitrii0777 <patrrriot991@gmail.com>
2025-10-05 17:21:13 +0300

    Add fa icons for both buttons

--- src/classes/RandomQuotesApp.js
+++ src/classes/RandomQuotesApp.js
@@ -23,19 +23,21 @@ class RandomQuoteApp {
     }
   }
 
-  getRandomQuote() {
+  randomQuoteHandler() {
     this.changeCurrentQuote(RandomQuote.getRandomQuote());
   }
 
-  async getRandomQuoteViaAPI() {
+  async randomQuoteViaAPIHandler() {
     const quoteViaAPI = await RandomQuote.getRandomQuoteViaAPI();
     this.changeCurrentQuote(quoteViaAPI);
   }
 
   init() {
-    this.randomQuoteBtn.addEventListener("click", () => this.getRandomQuote());
+    this.randomQuoteBtn.addEventListener("click", () =>
+      this.randomQuoteHandler()
+    );
     this.randomQuoteAPIBtn.addEventListener("click", () =>
-      this.getRandomQuoteViaAPI()
+      this.randomQuoteViaAPIHandler()
     );
   }
 }
