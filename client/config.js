4d25eb1f23d7244882fa974bca45ddc7cb0c5736
Dmitrii0777 <patrrriot991@gmail.com>
2025-10-06 18:14:24 +0300

    Move config.js to client folder

---
+++ client/config.js
@@ -0,0 +1,7 @@
+const config = {
+  PUBLIC_API_URL: "https://quoteslate.vercel.app",
+  API_URL: "https://random-quotes-oop-xs1a.onrender.com",
+  // API_URL: "http://localhost:3000",
+};
+
+export default config;
