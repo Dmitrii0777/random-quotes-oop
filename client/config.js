495efd9fab963cb72ea28285293a4ecafae6cebb
Dmitrii0777 <patrrriot991@gmail.com>
2025-10-06 18:37:37 +0300

    Changes to the config URL

--- client/config.js
+++ client/config.js
@@ -1,7 +1,6 @@
 const config = {
   PUBLIC_API_URL: "https://quoteslate.vercel.app",
-  API_URL: "https://random-quotes-oop-xs1a.onrender.com",
-  // API_URL: "http://localhost:3000",
+  API_URL: "http://localhost:3000",
 };
 
 export default config;
